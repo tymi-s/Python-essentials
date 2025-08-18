9368afc3290035cdc4c4decec685addb0dff75b4
tymi <tymonszyler@gmail.com>
2025-08-18 21:10:44 +0200

    com

---
+++ string-formating.py
@@ -0,0 +1,6 @@
+x=2020
+print(f"I am in {x} club")
+
+
+myhight = 180
+print(f"I am {'high' if myhight>=180 else 'short'}")
---
+++ virtual-env.py
@@ -0,0 +1,17 @@
+# how to create:
+
+"""
+CREATING:
+python -m venv ENVNAME
+
+ACTIVATING:
+
+ENVNAME\Scripts\activate
+
+
+DEACTIVATING:
+
+deactivate
+"""
+import cowsay
+cowsay.cow("HEJ MYSIA!")
